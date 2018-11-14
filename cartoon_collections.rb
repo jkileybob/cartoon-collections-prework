264b3fd47d3b8aa6fa51bb0ac9960471f4cce6b6
Kiley Bobbitt <jkileybob@aol.com>
2018-11-14 16:13:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
   array.collect do |word|
   word.capitalize + "!"
-  end
+end
 end
 
 def long_planeteer_calls# code an argument here
