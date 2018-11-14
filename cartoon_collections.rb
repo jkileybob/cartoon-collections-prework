7cae1a9979987eaa18f9f0e15536097b80a6f8f2
Kiley Bobbitt <jkileybob@aol.com>
2018-11-14 16:10:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,10 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index{|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.each do |exclaim|
+    exclaim << "!"
 end
 
 def long_planeteer_calls# code an argument here
