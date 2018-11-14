34a4f8b4c12491c11d8783e243b194ac32b90f20
Kiley Bobbitt <jkileybob@aol.com>
2018-11-14 16:29:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,11 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese(array)# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |cheese|
     cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    
   end  
 end
