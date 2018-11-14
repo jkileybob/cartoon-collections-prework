71c18aee3ee5596e701fd8b88bd51ac3740e518d
Kiley Bobbitt <jkileybob@aol.com>
2018-11-14 16:17:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.include? do |word|
+  array.any? do |word|
     word.length > 4
-  end  
+  end
 end
 
 def find_the_cheese# code an argument here
